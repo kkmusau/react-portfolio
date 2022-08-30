97a78accef219526ac8bbf5dc8bc32c8d0ff93b0
kkmusau <kkmusau@gmail.com>
2022-08-29 20:59:56 -0500

    added nav anf footer to app.js

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,15 @@
+import './Nav.css';
+
+const Nav = () => 
+    <nav>
+        <h1>Kaluki Musau</h1>
+
+        <div>
+            <button> About </button>
+            <button> Project </button>
+            <button> Contact </button>
+            <button> Resume </button>
+        </div>
+    </nav>
+
+export default Nav;
